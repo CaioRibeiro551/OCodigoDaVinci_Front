bb30c69ebf2fa339d99926ace48ef6059bc552f2
Caio <caio.h.r@outlook.com>
2023-09-15 17:36:36 -0300

    <FEAT> : <Adicionando componente Table na pasta coponents `.Table .components`>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Sidebar from '../../components/Sidebar/index';
+import Table from '../../components/Table/index';
 
 export default function Home() {
   return (
     <>
       <Sidebar />
+      <Table />
     </>
   );
 }
