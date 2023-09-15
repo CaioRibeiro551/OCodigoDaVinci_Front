0509d0d7d51221f6747cfc34241fe43956184d4d
Marcos-Samuel <marcos_samueloff@outlook.com>
2023-09-15 19:17:44 -0300

    <FIX>[resolvendo conflito]<bug do deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./router";
 import "./styles/index.css";
-import "./styles/form.css";
 import "./styles/button.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
