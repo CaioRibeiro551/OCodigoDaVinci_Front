5ad67e08db044430ce40b7dee1b9c5f1937f12ce
Vinícius Peter <vinicius.peter@outlook.com>
2023-09-15 22:43:28 -0300

    <Fix : <Ajustando pastas >

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,32 +3,55 @@ import "./style.css";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  const [eye, setEye] = useState(false);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
   const navigator = useNavigate();
 
+  const handleSetuser = ({ target }) => {
+    const key = target.name;
+    const value = target.value;
+    setUser({ ...user, [key]: value });
+  };
+
+  const handleSubmit = () => {
+    navigator("/home");
+    return;
+  };
+
   return (
-    <div className="signin-container">
+    <div className="signin-container max-width">
       <div className="signin-container-left"></div>
       <div className="signin-container-rigth">
-        <form action="">
+        <form>
           <h1>Faça seu login!</h1>
 
           <div className="container-inputs">
             <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" placeholder="Digite seu email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Digite seu email"
+              onChange={handleSetuser}
+            />
           </div>
 
           <div className="container-inputs">
-            <label htmlFor="senha">Senha</label>
+            <label htmlFor="password">Senha</label>
             <input
-              type={eye ? "text" : "password"}
-              name="senha"
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleSetuser}
               placeholder="Digite sua senha"
             />
-            <div className="password-toggle" onClick={() => setEye(!eye)}></div>
           </div>
 
-          <button onClick={() => navigator("/home")}>Entrar</button>
+          <button type="button" onClick={handleSubmit}>
+            Entrar
+          </button>
           <p>
             Ainda não possui conta?<Link to="/signup"> Cadastre-se</Link>
           </p>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,166 +1,199 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  Button,
-  Stepper,
-  Step,
-  StepLabel,
-  Box,
-  StepContent,
-  Typography,
-} from "@mui/material";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
-
+import eyeOpen from "../../assets/eye-open.png";
+import eyeClose from "../../assets/eye-close.png";
 import Sucess from "../../assets/sucess.svg";
+import Confirm from "../../assets/confirm-step.svg";
+import Active from "../../assets/active-step.svg";
+import StopSet from "../../assets/stop-step.svg";
 
-const steps = [
-  {
-    label: "Cadastre-se",
-    description: `Por favor, escreva seu nome e e-mail`,
-  },
-  {
-    label: "Escolha uma senha",
-    description: "Escolha uma senha segura",
-  },
-  {
-    label: "Cadastro realizado com sucesso",
-    description: `E-mail e senha cadastradoscom sucesso`,
-  },
-];
-
-export default function SignIn() {
-  const [activeStep, setActiveStep] = useState(0);
+export default function SignUp() {
+  const [stepIndex, setStepIndex] = useState(0);
+  const [imgStep2, setImgStep2] = useState(StopSet);
+  const [imgStep3, setImgStep3] = useState(StopSet);
   const [eye, setEye] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const navigator = useNavigate();
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  function stepImg() {
+    if (stepIndex === 1) {
+      setImgStep2(Active);
+    }
+    if (stepIndex === 2) {
+      setImgStep2(Confirm);
+      setImgStep3(Confirm);
+    }
+    if (stepIndex === 3) {
+      setImgStep3(Confirm);
+    }
+  }
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    setStepIndex(stepIndex + 1);
+    stepImg();
+    return;
   };
 
+  const handleSetuser = ({ target }) => {
+    const key = target.name;
+    const value = target.value;
+    setUser({ ...user, [key]: value });
+    console.log(user);
+  };
+
+  useEffect(() => {
+    stepImg();
+  }, [stepIndex]);
+
   return (
-    <div className="signup-container">
+    <div className="signup-container max-width">
       <div className="signup-container-left">
-        <div>
-          <Stepper activeStep={activeStep} orientation="vertical">
-            {steps.map((label) => (
-              <Step key={label.label}>
-                <StepLabel>{label.label} </StepLabel>
-                <StepContent>
-                  <Typography>{label.description}</Typography>
-                </StepContent>
-              </Step>
-            ))}
-          </Stepper>
+        <div className="container-stepper">
+          <div className="container-step">
+            <div className="container-step-title">
+              <img src={stepIndex === 0 ? Active : Confirm} />
+              <span className="title-stepl">Cadastre-se</span>
+            </div>
+
+            <span className="description">
+              Por favor, escreva seu nome e e-mail
+            </span>
+          </div>
+
+          <div className="container-step">
+            <div className="container-step-title">
+              <img src={imgStep2} />
+              <span className="title-stepl">Escolha uma senha</span>
+            </div>
+            <span className="description">Escolha uma senha segura</span>
+          </div>
+
+          <div className="container-step">
+            <div className="container-step-title">
+              <img src={imgStep3} />
+              <span className="title-stepl">
+                Cadastro realizado com sucesso
+              </span>
+            </div>
+            <span className="description">
+              E-mail e senha cadastrados com sucesso
+            </span>
+          </div>
         </div>
       </div>
 
       <div className="signup-container-rigth">
-        <form>
-          {activeStep === 0 && (
-            <div>
-              <form action="">
-                <h1>Adicione seus dados</h1>
+        <>
+          {stepIndex === 0 && (
+            <form action="">
+              <h1>Adicione seus dados</h1>
 
-                <div className="container-inputs">
-                  <label htmlFor="name">Nome *</label>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Digite seu nome"
-                  />
-                </div>
+              <div className="container-inputs">
+                <label htmlFor="name">Nome *</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={handleSetuser}
+                  placeholder="Digite seu nome"
+                />
+              </div>
 
-                <div className="container-inputs">
-                  <label htmlFor="email">Email *</label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Digite seu email"
-                  />
-                </div>
+              <div className="container-inputs">
+                <label htmlFor="email">Email *</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={handleSetuser}
+                  placeholder="Digite seu email"
+                />
+              </div>
 
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
-                >
-                  Próximo
-                </Button>
-                <Link to="/signin">Já possui uma conta? Faça seu Login</Link>
-              </form>
-            </div>
+              <button type="button" onClick={handleNext}>
+                Continuar
+              </button>
+
+              <p>
+                Já possui uma conta? Faça seu <Link to="/"> Login</Link>
+              </p>
+            </form>
           )}
 
-          {activeStep === 1 && (
+          {stepIndex === 1 && (
             <div>
               <form action="">
                 <h1>Escolha uma senha</h1>
 
                 <div className="container-inputs">
-                  <label htmlFor="senha">Senha *</label>
+                  <label htmlFor="password">Senha *</label>
                   <input
                     type={eye ? "text" : "password"}
-                    name="Senha"
+                    name="password"
+                    id="password"
+                    onChange={handleSetuser}
                     placeholder="Digite seu senha"
                   />
-                  <div
-                    className="password-toggle"
-                    onClick={() => setEye(!eye)}
-                  ></div>
+                  <div className="password-toggle" onClick={() => setEye(!eye)}>
+                    {eye ? <img src={eyeOpen} /> : <img src={eyeClose} />}
+                  </div>
                 </div>
 
                 <div className="container-inputs">
-                  <label htmlFor="repeteSenha">Repita a senha *</label>
+                  <label htmlFor="confirmPassword">Repita a senha *</label>
                   <input
                     type={eye ? "text" : "password"}
-                    name="repeteSenha"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    onChange={handleSetuser}
                     placeholder="Repita a senha"
                   />
-                  <div
-                    className="password-toggle"
-                    onClick={() => setEye(!eye)}
-                  ></div>
+                  <div className="password-toggle" onClick={() => setEye(!eye)}>
+                    {eye ? <img src={eyeOpen} /> : <img src={eyeClose} />}
+                  </div>
                 </div>
 
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
-                >
-                  Próximo
-                </Button>
-                <Link to="/signin">Já possui uma conta? Faça seu Login</Link>
+                <button type="button" onClick={handleNext}>
+                  Finalizar cadastro
+                </button>
+                <p>
+                  Já possui uma conta? Faça seu <Link to="/"> Login</Link>
+                </p>
               </form>
             </div>
           )}
 
-          {activeStep === 2 && (
+          {stepIndex === 2 && (
             <div className="container-finish">
               <div className="finish">
                 <img src={Sucess} alt="finish" />
                 <h2>Cadastro realizado com sucesso</h2>
               </div>
 
-              <button onClick={() => navigator("/signin")}>
-                Ir para o login
-              </button>
+              <button onClick={() => navigator("/")}>Ir para o login</button>
             </div>
           )}
-        </form>
-        <div>
-          <Box display="flex" justifyContent="center" mt={3}>
-            {steps.map((_, index) => (
-              <Box
-                key={index}
-                width={50}
-                height={5}
-                bgcolor={index <= activeStep ? "success.main" : "grey.400"}
-                ml={index > 0 ? 2 : 0}
-              />
-            ))}
-          </Box>
+        </>
+
+        <div className="container-highlighter">
+          <div
+            className={`highlighter ${stepIndex === 0 && "highlighter-active"}`}
+          ></div>
+          <div
+            className={`highlighter ${stepIndex === 1 && "highlighter-active"}`}
+          ></div>
+          <div
+            className={`highlighter ${stepIndex === 2 && "highlighter-active"}`}
+          ></div>
         </div>
       </div>
     </div>
