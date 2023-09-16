8390e337681a86a08c399163105f96e46ca28957
Caio <caio.h.r@outlook.com>
2023-09-16 15:24:31 -0300

    <FEAT> : <Estilizando pagina home `.Home`>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,28 @@
-import Sidebar from "../../components/Sidebar/index";
-import Table from "../../components/Table/index";
-import TituloCobranca from "../../components/TitutloCobranca/index";
+import React from 'react';
+import Sidebar from '../../components/Sidebar/index';
+// import Table from '../../components/Table/index';
+// import TituloCobranca from '../../components/TituloCobranca/index';
+// import '../../styles/index.css';
+import './style.css';
+import seta from '../../assets/seta.svg';
+import cicleInfo from '../../assets/circle-info.svg';
 
 export default function Home() {
   return (
-    <>
+    <div className="page-container">
+      <header className="full-header">
+        <div className="header-info">
+          <h1>Resumo das cobranças</h1>
+        </div>
+        <div className="user-info">
+          <img className="circle-info" src={cicleInfo} alt="" />
+          <p>Lorena</p>
+          <img src={seta} alt="Seta" />
+        </div>
+      </header>
       <Sidebar />
-      <Table />
-      <TituloCobranca />
-    </>
+      {/* <Table />
+      <TituloCobranca /> */}
+    </div>
   );
 }
