d832f4bb1bd64bbeab155778d34a8301cdd75943
Caio <caio.h.r@outlook.com>
2023-09-17 12:19:05 -0300

    <FEAT> : <Componentizando o Headerda home `.components`>

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+import seta from '../../assets/seta.svg';
+import cicleInfo from '../../assets/circle-info.svg';
+import iconeEdit from '../../assets/icone-edit.svg';
+import iconeQuit from '../../assets/icone-quit.svg';
+import iconePolygon from '../../assets/icone-polygon.svg';
+import './style.css';
+
+export default function Header() {
+  return (
+    <>
+      <header className="full-header">
+        <div className="header-info">
+          <h1>Resumo das cobranças</h1>
+        </div>
+        <div className="user-info">
+          <img className="circle-info" src={cicleInfo} alt="" />
+          <p>Lorena</p>
+          <img src={seta} alt="Seta" />
+        </div>
+        <img className="polygon-item" src={iconePolygon} alt="" />
+        <div className="button-edit">
+          <div className="button-item">
+            <img src={iconeEdit} alt="" />
+            <p>Editar</p>
+          </div>
+          <div className="button-item">
+            <img src={iconeQuit} alt="" />
+            <p>Sair</p>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,37 +2,12 @@ import React from 'react';
 import Sidebar from '../../components/Sidebar/index';
 import Table from '../../components/Table/index';
 import TituloCobranca from '../../components/TituloCobranca/index';
-import './style.css';
-import seta from '../../assets/seta.svg';
-import cicleInfo from '../../assets/circle-info.svg';
-import iconeEdit from '../../assets/icone-edit.svg';
-import iconeQuit from '../../assets/icone-quit.svg';
-import iconePolygon from '../../assets/icone-polygon.svg';
+import Header from '../../components/Header/index';
 
 export default function Home() {
   return (
     <div className="page-container">
-      <header className="full-header">
-        <div className="header-info">
-          <h1>Resumo das cobranças</h1>
-        </div>
-        <div className="user-info">
-          <img className="circle-info" src={cicleInfo} alt="" />
-          <p>Lorena</p>
-          <img src={seta} alt="Seta" />
-        </div>
-        <img className="polygon-item" src={iconePolygon} alt="" />
-        <div className="button-edit">
-          <div className="button-item">
-            <img src={iconeEdit} alt="" />
-            <p>Editar</p>
-          </div>
-          <div className="button-item">
-            <img src={iconeQuit} alt="" />
-            <p>Sair</p>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="main-container">
         <Sidebar />
         <div className="card-container">
