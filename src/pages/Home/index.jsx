41ba528170293ac9ae9ca22ef808ba31041c4465
Caio <caio.h.r@outlook.com>
2023-09-17 06:49:54 -0300

    <FEAT> : <Adicionando botão de editar usuário `.Home`>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import Sidebar from "../../components/Sidebar/index";
-import Table from "../../components/Table/index";
-import TituloCobranca from "../../components/TituloCobranca/index";
-import "./style.css";
-import seta from "../../assets/seta.svg";
-import cicleInfo from "../../assets/circle-info.svg";
+import React from 'react';
+import Sidebar from '../../components/Sidebar/index';
+import Table from '../../components/Table/index';
+import TituloCobranca from '../../components/TituloCobranca/index';
+import './style.css';
+import seta from '../../assets/seta.svg';
+import cicleInfo from '../../assets/circle-info.svg';
+import iconeEdit from '../../assets/icone-edit.svg';
+import iconeQuit from '../../assets/icone-quit.svg';
+import iconePolygon from '../../assets/icone-polygon.svg';
 
 export default function Home() {
   return (
@@ -17,6 +20,17 @@ export default function Home() {
           <img className="circle-info" src={cicleInfo} alt="" />
           <p>Lorena</p>
           <img src={seta} alt="Seta" />
+        </div>
+        <img className="polygon-item" src={iconePolygon} alt="" />
+        <div className="button-edit">
+          <div className="button-item">
+            <img src={iconeEdit} alt="" />
+            <p>Editar</p>
+          </div>
+          <div className="button-item">
+            <img src={iconeQuit} alt="" />
+            <p>Sair</p>
+          </div>
         </div>
       </header>
       <main className="main-container">
