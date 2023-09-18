4c34481c1fafa5bf8ec35c595596e53300e698dd
Vinícius Peter <vinicius.peter@outlook.com>
2023-09-18 19:29:41 -0300

    <FEAT> : <Atualização do perfil do usuario>

--- src/components/ModalTeste/index.jsx
+++ src/components/ModalTeste/index.jsx
@@ -1,13 +1,50 @@
 import "./style.css";
 import CloseModal from "../../assets/close.svg";
 import { useMainContext } from "../../hooks/useMainContext";
+import { useEffect, useState } from "react";
+import Axios from "../../services/api";
 
 export default function Modal() {
-  const { setModalTeste } = useMainContext();
+  const { setModalTeste, userLog, setUserLog } = useMainContext();
+  const [form, setForm] = useState({
+    name: userLog.name,
+    email: userLog.email,
+    cpf: userLog.cpf.trim(),
+    phone: userLog.phone,
+  });
+  const [currentDados, setCurrentDados] = useState({});
+
+  const handleChange = ({ target }) => {
+    const key = target.name;
+    const value = target.value;
+    setCurrentDados({ ...form, [key]: value });
+    return;
+  };
 
   const handleCloseModal = () => {
     setModalTeste(false);
   };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await Axios.patch("/update-me", form, {
+        headers: {
+          Authorization: userLog.token,
+        },
+      });
+      alert("Usuário atualizado com sucesso!");
+      setUserLog({ ...userLog, ...form });
+      handleCloseModal();
+      return;
+    } catch (error) {
+      alert(error.response.data.message);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    setForm(currentDados);
+  }, [currentDados]);
 
   return (
     <>
@@ -18,37 +55,69 @@ export default function Modal() {
 
           <div className="container-inputs">
             <label htmlFor="nome">Nome *</label>
-            <input type="text" name="nome" placeholder="Digite seu nome" />
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Digite seu nome"
+            />
           </div>
 
           <div className="container-inputs">
             <label htmlFor="email">E-mail *</label>
-            <input type="email" name="email" placeholder="Digite seu email" />
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Digite seu email"
+            />
           </div>
 
           <div className="container-cpf-telefone">
             <div className="container-inputs">
-              <label htmlFor="email">CPF</label>
-              <input type="email" name="email" placeholder="Digite seu CPF" />
+              <label htmlFor="cpf">CPF</label>
+              <input
+                type="text"
+                name="cpf"
+                value={form.cpf}
+                onChange={handleChange}
+                placeholder="Digite seu CPF"
+              />
             </div>
             <div className="container-inputs">
-              <label htmlFor="email">Telefone</label>
+              <label htmlFor="telefone">Telefone</label>
               <input
-                type="email"
-                name="email"
+                type="text"
+                name="phone"
+                value={form.phone}
+                onChange={handleChange}
                 placeholder="Digite seu telefone"
               />
             </div>
           </div>
           <div className="container-inputs">
-            <label htmlFor="email">Nova senha</label>
-            <input type="email" name="email" placeholder="Digite sua senha" />
+            <label htmlFor="newPassword">Nova senha</label>
+            <input
+              type="password"
+              name="newPassword"
+              onChange={handleChange}
+              placeholder="Digite sua senha"
+            />
           </div>
           <div className="container-inputs">
-            <label htmlFor="email">Confirmar senha</label>
-            <input type="email" name="email" placeholder="Confirmar senha" />
+            <label htmlFor="password">Confirmar senha</label>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              placeholder="Confirmar senha"
+            />
           </div>
-          <button>Atualizar</button>
+          <button type="button" onClick={handleSubmit}>
+            Atualizar
+          </button>
         </form>
       </div>
     </>
