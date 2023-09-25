0942acbcd94bc9f465a2a63818064379b7ac02c4
Caio <caio.h.r@outlook.com>
2023-09-25 18:24:48 -0300

    <FEAT> : <Validações modal usuárioI> ` .ModalEditUser`>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./style.css";
-import seta from "../../assets/seta.svg";
-import cicleInfo from "../../assets/circle-info.svg";
-import iconeEdit from "../../assets/icone-edit.svg";
-import iconeQuit from "../../assets/icone-quit.svg";
-import iconePolygon from "../../assets/icone-polygon.svg";
-import { useMainContext } from "../../hooks/useMainContext";
-import { useNavigate, useLocation } from "react-router-dom";
-import Modal from "../ModalTeste/index";
-import ConfirmationModal from "../ConfirmationModal";
+import React, { useState } from 'react';
+import './style.css';
+import seta from '../../assets/seta.svg';
+import cicleInfo from '../../assets/circle-info.svg';
+import iconeEdit from '../../assets/icone-edit.svg';
+import iconeQuit from '../../assets/icone-quit.svg';
+import iconePolygon from '../../assets/icone-polygon.svg';
+import { useMainContext } from '../../hooks/useMainContext';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Modal from '../../components/ModalEditUser';
+import ConfirmationModal from '../ConfirmationModal';
 
 export default function Header({ title, subtitle }) {
   const { setModalTeste, removeUserLog, userLog, modalTeste } =
@@ -33,7 +33,7 @@ export default function Header({ title, subtitle }) {
 
   const confirmLogout = () => {
     removeUserLog();
-    navigator("/");
+    navigator('/');
 
     setIsConfirmationModalOpen(false);
   };
@@ -47,15 +47,15 @@ export default function Header({ title, subtitle }) {
       <header>
         <div className="contents-header">
           <div className="header-info">
-            {pathname === "/home" ? (
+            {pathname === '/home' ? (
               <h1 className="big-text">{title}</h1>
             ) : (
               <p className="text-client">
-                {title}{" "}
+                {title}{' '}
                 {subtitle && (
                   <span>
-                    {" "}
-                    <span className="separar-title">{">"}</span> {subtitle}
+                    {' '}
+                    <span className="separar-title">{'>'}</span> {subtitle}
                   </span>
                 )}
               </p>
@@ -86,7 +86,7 @@ export default function Header({ title, subtitle }) {
           </div>
         </div>
       </header>
-      {modalTeste ? <Modal /> : ""}
+      {modalTeste ? <Modal /> : ''}
 
       <ConfirmationModal
         isOpen={isConfirmationModalOpen}
---
+++ src/validation/validationEditUser.jsx
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+export const validationEditUser = yup.object().shape({
+  name: yup.string().required('Campo obrigatório'),
+  email: yup.string().email('E-mail inválido').required('Campo obrigatório'),
+  cpf: yup.string().required('Campo obrigatório'),
+  phone: yup.string().required('Campo obrigatório'),
+  newPassword: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
+  password: yup
+    .string()
+    .oneOf([yup.ref('newPassword'), null], 'As senhas não coincidem'),
+});
