52a3a36d5f6b8afa0c1ccbf114193667cd87ce07
Caio <caio.h.r@outlook.com>
2023-09-27 15:33:46 -0300

    <FEAT> : <Adicionando mensagem de excluir cobrança a componentess> ` .componentss`>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,35 @@
-import Modal from "../../components/ModalEditUser";
-import Sidebar from "../../components/Sidebar/index";
-import TituloCobranca from "../../components/TituloCobranca";
-import Header from "../../components/Header/index";
-import { useMainContext } from "../../hooks/useMainContext";
-import "./style.css";
-import { useState, useEffect } from "react";
-import Axios from "../../services/api";
-import pagasImg from "../../assets/pagas.svg";
-import vencidasImg from "../../assets/vencidas.svg";
-import previstasIMG from "../../assets/previstas.png";
-import { ResumeLargeTable } from "../../components/ResumeLargeTable";
-import ResumeSmallTable from "../../components/ResumeSmallTable";
+import Modal from '../../components/ModalEditUser';
+import Sidebar from '../../components/Sidebar/index';
+import TituloCobranca from '../../components/TituloCobranca';
+import Header from '../../components/Header/index';
+import { useMainContext } from '../../hooks/useMainContext';
+import './style.css';
+import { useState, useEffect } from 'react';
+import Axios from '../../services/api';
+import pagasImg from '../../assets/pagas.svg';
+import vencidasImg from '../../assets/vencidas.svg';
+import previstasIMG from '../../assets/previstas.png';
+import { ResumeLargeTable } from '../../components/ResumeLargeTable';
+import ResumeSmallTable from '../../components/ResumeSmallTable';
+import Menssage from '../../components/MensageExcluirCobranca';
 
 const headerTitle = {
-  header: "Resumo das cobranças",
-  emDia: "Clientes em dia",
-  Inadimplente: "Clientes Inadimplentes",
-  vencidas: "Cobranças Vencidas",
-  pagas: "Cobranças Pagas",
-  prevista: "Cobranças Previstas",
+  header: 'Resumo das cobranças',
+  emDia: 'Clientes em dia',
+  Inadimplente: 'Clientes Inadimplentes',
+  vencidas: 'Cobranças Vencidas',
+  pagas: 'Cobranças Pagas',
+  prevista: 'Cobranças Previstas',
 };
 
 export default function Home() {
-
   const { modalTeste, userLog } = useMainContext();
   const [clients, setClients] = useState([]);
   const [charges, setCharges] = useState([]);
 
   const getClients = async () => {
     try {
-      const { data } = await Axios.get("/clients", {
+      const { data } = await Axios.get('/clients', {
         headers: {
           Authorization: userLog.token,
         },
@@ -43,7 +43,7 @@ export default function Home() {
 
   const getCharges = async () => {
     try {
-      const { data } = await Axios.get("/charges", {
+      const { data } = await Axios.get('/charges', {
         headers: {
           Authorization: userLog.token,
         },
@@ -59,20 +59,20 @@ export default function Home() {
     getCharges();
   }, []);
 
-  const clientsEmDia = clients.filter((client) => client.status == "Em dia");
+  const clientsEmDia = clients.filter((client) => client.status == 'Em dia');
 
   const clientsVencidos = clients.filter(
-    (client) => client.status == "Inadimplente"
+    (client) => client.status == 'Inadimplente',
   );
 
-  const cobrancasPagas = charges.filter((charge) => charge.status == "Paga");
+  const cobrancasPagas = charges.filter((charge) => charge.status == 'Paga');
 
   const cobrancasVencidas = charges.filter(
-    (charge) => charge.status == "Vencida"
+    (charge) => charge.status == 'Vencida',
   );
 
   const cobrancasPrevistas = charges.filter(
-    (charge) => charge.status == "Pendente"
+    (charge) => charge.status == 'Pendente',
   );
 
   const totalVencidas = cobrancasPagas.reduce((total, objeto) => {
@@ -89,22 +89,22 @@ export default function Home() {
 
   const cards = [
     {
-      text: "Cobranças Pagas",
-      valor: `R$ ${totalPagas.toLocaleString("pt-BR")}`,
+      text: 'Cobranças Pagas',
+      valor: `R$ ${totalPagas.toLocaleString('pt-BR')}`,
       img: pagasImg,
-      color: "#EEF6F6",
+      color: '#EEF6F6',
     },
     {
-      text: "Cobranças Vencidas",
-      valor: `R$ ${totalVencidas.toLocaleString("pt-BR")}`,
+      text: 'Cobranças Vencidas',
+      valor: `R$ ${totalVencidas.toLocaleString('pt-BR')}`,
       img: vencidasImg,
-      color: "#FFEFEF",
+      color: '#FFEFEF',
     },
     {
-      text: "Cobranças Previstas",
-      valor: `R$ ${totalPrevistas.toLocaleString("pt-BR")}`,
+      text: 'Cobranças Previstas',
+      valor: `R$ ${totalPrevistas.toLocaleString('pt-BR')}`,
       img: previstasIMG,
-      color: "#FCF6DC",
+      color: '#FCF6DC',
     },
   ];
 
